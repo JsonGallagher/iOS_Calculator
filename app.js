f2c5b69e32afa713ed016c89beb316705eca43fe
Jason Gallagher <jasong13@gmail.com>
2025-11-23 16:00:39 -0700

    Added eslint rules to allow ES6

---
+++ app.js
@@ -0,0 +1,16 @@
+// grab elements
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+
+// basic calculator functions
+// the number the user is typing right now
+function currentValue() {}
+
+// stored number when an operator is chosen
+function previousValue() {}
+
+// operators
+function operator() {}
+
+// when true, next digit starts a new number (after pressing an operator or equals)
+function shouldResetDisplay() {}
